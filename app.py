d0f117406417489f6f6cafc45d421c9849158dfa
yishan2004931022-glitch <yishan2004931022@gmail.com>
2026-01-14 10:33:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("This app is built with Streamlit!")
 st.write("HAHAHAHA!")
 
 # reading the data
-link=""
+link="https://drive.google.com/file/d/1zdxtFQSRAeG50Vx5Rpj5pzW47UB3UJ7C/view?usp=drive_link"
 df=pdf.read_csv(link)
 
 # create a download data button for original dataset
@@ -28,6 +28,7 @@ st.line_chart(df.set_index("x"))
 st.header("Your first widget")
 number = st.slider("Pick a number", min_value=0, max_value=100, value=50)
 st.write("You picked:", number)
+
 
 
 
